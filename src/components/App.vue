e4f03daf82d62d5440d3587bef89474c0188650b
keupoz <keupoz@gmail.com>
2019-05-31 21:33:12 +0700

    Added tab switcher

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,14 +2,18 @@
   <div class="app" v-if="$app.ready">
     <Header />
     <Renderer />
-    <Form
+
+    <TabSwitcher :currentTab="currentTab" :tabs="tabs" @change="currentTab = $event" />
+
+    <Form v-show="currentTab.name == 'retriever'"
       placeholder = "Enter nickname"
 
       :lastError = "$app.lastRetrieveError"
       :loading   = "$app.loading"
 
       @submit="$app.retrieveSkin($event)" />
-    <Editor />
+    
+    <Editor v-show="currentTab.name == 'editor'" />
   </div>
 
   <Loader v-else />
@@ -18,12 +22,25 @@
 <script>
 import Loader from './Loader.vue'
 
-import Header   from './Header.vue'
-import Form     from './Form.vue'
-import Editor   from './Editor.vue'
-import Renderer from './Renderer.vue'
+import Header      from './Header.vue'
+import Form        from './Form.vue'
+import Editor      from './Editor.vue'
+import Renderer    from './Renderer.vue'
+import TabSwitcher from './TabSwitcher.vue'
 
 export default {
-  components: { Loader, Header, Form, Renderer, Editor }
+  components: { Loader, Header, Renderer, TabSwitcher, Form, Editor },
+
+  data () {
+    let tabs = [
+      { name: 'retriever', label: 'Skin retriever' },
+      { name: 'editor',    label: 'Trigger pixels' }
+    ];
+
+    return {
+      currentTab: tabs[0],
+      tabs
+    }
+  }
 }
 </script>
---
+++ src/components/TabSwitcher.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="tab-switcher">
+    <div
+      class="tab-switcher--tab"
+      v-for="tab in tabs"
+      :key="tab.name"
+      :class="{ selected: current == tab }"
+      
+      @click="setTab(tab)">{{ tab.label }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [ 'currentTab', 'tabs' ],
+
+  data () {
+    return {
+      current: this.currentTab
+    }
+  },
+
+  methods: {
+    setTab (tab) {
+      this.current = tab;
+      this.$emit('change', tab);
+    }
+  },
+
+  watch: {
+    'currentTab' (newVal) {
+      this.current = newVal;
+    }
+  }
+}
+</script>
