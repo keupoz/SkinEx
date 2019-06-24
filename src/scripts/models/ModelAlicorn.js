4d71b994125fed58195610692a9ed995a464bdd9
keupoz <keupoz@gmail.com>
2019-06-24 21:51:45 +0700

    Changed wings initialization way

--- src/scripts/models/ModelAlicorn.js
+++ src/scripts/models/ModelAlicorn.js
@@ -1,12 +1,15 @@
 import ModelUnicorn from './ModelUnicorn'
 
-import ModelWings   from './components/ModelWings'
-import WingRenderer from './components/WingRenderer'
+import ModelPegasus from './ModelPegasus';
 
 export default class ModelAlicorn extends ModelUnicorn {
   init () {
     super.init();
 
-    this.wings = this.addSub('wings', ModelWings, WingRenderer);
+    this.initWings   = ModelPegasus.prototype.initWings;
+    this.foldWings   = ModelPegasus.prototype.foldWings;
+    this.unfoldWings = ModelPegasus.prototype.unfoldWings;
+
+    this.initWings();
   }
 }
--- src/scripts/models/ModelPegasus.js
+++ src/scripts/models/ModelPegasus.js
@@ -1,12 +1,27 @@
 import ModelEarthpony from './ModelEarthpony'
 
-import ModelWings   from './components/ModelWings'
 import WingRenderer from './components/WingRenderer'
 
 export default class ModelPegasus extends ModelEarthpony {
   init () {
     super.init();
+    this.initWings();
+  }
 
-    this.addSub('wings', ModelWings, WingRenderer);
+  initWings () {
+    this.addSub('leftWing',  WingRenderer, false, 32);
+    this.addSub('rightWing', WingRenderer, true,  16);
+
+    this.foldWings();
+  }
+
+  foldWings () {
+    this.getSub('leftWing') .fold();
+    this.getSub('rightWing').fold();
+  }
+
+  unfoldWings () {
+    this.getSub('leftWing') .unfold();
+    this.getSub('rightWing').unfold();
   }
 }
