a91a1f9bf3be82bee8d498baa9c3ae18ca2887bd
keupoz <keupoz@gmail.com>
2019-06-27 22:33:06 +0700

    Removed MC4EP server

--- src/scripts/config/servers.js
+++ src/scripts/config/servers.js
@@ -1,7 +1,6 @@
 export default function registerServers (servers) {
   servers
     .setRetrieverURL('https://keupoz.herokuapp.com/ponyskins/%endpoint%/%nickname%')
-    .registerServer('mc4ep',    'MC4EP', true)
     .registerServer('valhalla', 'Valhalla')
     .registerServer('legacy',   'Legacy')
     .registerServer('mojang',   'Mojang')
