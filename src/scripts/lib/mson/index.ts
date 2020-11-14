74458575b30cb812ee94b2292fc387a532eb5a4b
keupoz <keupoz@gmail.com>
2020-11-14 12:26:21 +0700

    Added credit

--- src/scripts/lib/mson/index.ts
+++ src/scripts/lib/mson/index.ts
@@ -1,2 +1,10 @@
+/**
+ * Thanks to Sollace for implementing Mson core in JS
+ * https://github.com/MineLittlePony/Mson/tree/1.16/js
+ *
+ * @author Sollace <https://github.com/Sollace>
+ * @author Keupoz <https://github.com/Keupoz>
+ */
+
 import "./internals";
 export { createLoader } from "./loader";
